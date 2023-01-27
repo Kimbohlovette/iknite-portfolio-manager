3d4004a5b17a3695445d718419ef45c10e8016af
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2023-01-27 06:04:33 +0100

    navbar links working without page refreshes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,55 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from "react-router-dom";
+
 import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Projects from "./pages/Projects";
 import Members from './pages/Members';
 import Iknite from "./pages/Iknite";
 
 
-const router = createBrowserRouter([
-       { path: '/', element: <Home />},
-       { path: '/members', element: <Members />},     
-       { path: '/projects', element: <Projects />},     
-       { path: '/iknite', element: <Iknite />},     
-])
+
+const MyApp = (): JSX.Element =>{
+        return (
+            <div>
+                <NavBar />
+                <div className="px-4 sm:px-8 lg:px-12">
+                    <Outlet />
+                </div>
+                <Footer />
+            </div>);
+        }
+
+// const routeDefinitions = createRoutesFromElements(
+//     <Route element={<MyApp />}>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/projects' element={<Projects />} />
+//         <Route path='/members' element={<Members />} />
+//         <Route path='/iknite' element={<Iknite />} />
+//     </Route>
+// )
+// const router = createBrowserRouter(routeDefinitions)
+
+const router = createBrowserRouter(
+    [
+        { 
+            element: <MyApp />,
+            children: [
+                {   path: '/', element: <Home />},
+                {   path: '/projects', element: <Projects />},
+                {   path: '/members', element: <Members />},
+                {   path: '/iknite', element: <Iknite />},
+            ] 
+        },
+    ]
+);
 
 function App() {
 
   return (
     <div>
-        <NavBar />
-        <div className="px-4 sm:px-8 lg:px-12">
-            <RouterProvider router={router} />
-        </div>
-        <Footer />
+        <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar(): JSX.Element {
 
@@ -8,31 +9,24 @@ export default function NavBar(): JSX.Element {
     <header className="flex flex-row justify-between items-center bg-purple-900 py-4 text-white px-4 sm:px-8">
         <div className="brand text-2xl font-bold">Brand</div>
         <nav className="hidden sm:block">
-            <ul className="flex flex-row gap-4 font-medium [&>*]:px-2 [&>*]:py-1">
-                <a href="/">
-                <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 ">
-                <Icon icon="material-symbols:add-home" className="inline-block mr-2 text-base" />
-                Home</li>
-                </a>
-                <a href="./projects">
-                <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 ">
-                <Icon icon="grommet-icons:projects" className="inline-block mr-2 text-sm" />
-                Projects</li>
-                </a>
-                <a href="/members">
-                <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 ">
-                <Icon icon="ri:team-fill" className="inline-block mr-2 text-base" />
-                Members</li>
-                </a>
-                <a href="/iknite">
-                <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 ">
-                <Icon icon="mdi:about"  className="inline-block mr-2 text-base" />
-                About Iknite</li>
-                </a>
+            <ul className="flex flex-row gap-4 font-medium">
+                <Link to="/">
+                    <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 px-4 py-2">Home</li>
+                </Link>
+                <Link to="/projects">
+                    <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 px-4 py-2">Projects</li>
+                </Link>
+
+                <Link to="/members">
+                    <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 px-4 py-2">Members</li>
+                </Link>
+                <Link to="/iknite">
+                    <li className="hover:bg-purple-100/80 hover:backdrop-blur-sm rounded-md hover:text-slate-800 px-4 py-2">Iknite</li>
+                </Link>
             </ul>
         </nav>
         <div className="auth hidden  sm:flex flex-row items-center gap-8">
-            <button className="py-1 px-4 rounded-md bg-white text-slate-800 text-sm">Login</button>
+            <button className="py-2 px-4 rounded-md bg-white text-slate-800 text-sm">Login</button>
             <div className="avatar p-2 rounded-full text-xl bg-white">
             <Icon icon="mdi:user" className='text-purple-900'/>
             </div>
