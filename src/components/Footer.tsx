3dfda1e695f8699a8c4ff022e0ecc2089bffe8be
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2023-02-11 21:42:53 +0100

    Change footer heading text-color and links on hover color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,30 +5,30 @@ function Footer() {
     <footer className="py-8 px-4 sm:px-8 lg:px-12 bg-primary-900 text-slate-200">
         <div className="flex flex-col md:flex-row">
             <div className="sitemap flex-1">
-                <h1 className="py-3 text-lg font-semibold">Sitemap</h1>
+                <h1 className="py-3 text-lg font-semibold text-secondary-600">Sitemap</h1>
                 <ul className="flex flex-col gap-2 text-sm font-light">
-                    <Link to="/"><li className="hover:text-slate-300">Home</li></Link>
-                    <Link to="/projects"><li className="hover:text-slate-300">Projects</li></Link>
+                    <Link to="/"><li className="hover:text-secondary-400">Home</li></Link>
+                    <Link to="/projects"><li className="hover:text-secondary-400">Projects</li></Link>
                     <Link to="/members"><li className="hover:text-slate-300">Members</li></Link>
-                    <Link to="/iknite"><li className="hover:text-slate-300">About Iknite</li></Link>
+                    <Link to="/iknite"><li className="hover:text-secondary-400">About Iknite</li></Link>
                 </ul>
             </div>
             <div className="socials flex-1">
-                <h1 className="py-3 text-lg font-semibold">Follow Us</h1>
+                <h1 className="py-3 text-lg font-semibold text-secondary-600">Follow Us</h1>
                 <ul className="flex flex-col gap-2 text-sm font-light">
-                    <a href="#home"><li className="hover:text-slate-300">LinkedIn</li></a>
-                    <a href="#home"><li className="hover:text-slate-300">Twitter</li></a>
-                    <a href="#home"><li className="hover:text-slate-300">Facebook</li></a>
-                    <a href="#home"><li className="hover:text-slate-300">Official website</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">LinkedIn</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">Twitter</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">Facebook</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">Official website</li></a>
                 </ul>
             </div>
             <div className="partners flex-1">
-                <h1 className="py-3 text-lg font-semibold">Partners</h1>
+                <h1 className="py-3 text-lg font-semibold text-secondary-600">Partners</h1>
                 <ul className="flex flex-col gap-2 text-sm font-light">
-                    <a href="#home"><li className="hover:text-slate-300">Mountain Hub</li></a>
-                    <a href="#home"><li className="hover:text-slate-300">MOCU</li></a>
-                    <a href="#home"><li className="hover:text-slate-300">Kuelabs</li></a>
-                    <a href="#home"><li className="hover:text-slate-300">Iknite Studio</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">Mountain Hub</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">MOCU</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">Kuelabs</li></a>
+                    <a href="#home"><li className="hover:text-secondary-400">Iknite Studio</li></a>
                 </ul>
             </div>
         </div>
